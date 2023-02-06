06b5729371b8bb57cdb36c852922cc396f7a22c3
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-06 15:17:27 +0200

    task3

--- script.js
+++ script.js
@@ -20,3 +20,12 @@ let c = prompt('Enter a number');
    }
    alert(convertNubmer(c));
 
+let number = prompt('enter a number');
+let count = prompt('one more number, please');
+function three(number, count){
+for(let i = 0; i < count; i++){
+   number +=3;
+}
+return number;
+}
+alert(three(+number, +count));
