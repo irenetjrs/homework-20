7217363d7d1a2162d87c008a910ec04b57718896
irenetjrs <irkapidgorodetska@gmail.com>
2023-02-06 14:42:28 +0200

    first task

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-function maxNumber (a, b){
    let a = prompt(`Type the number below`);
    let b = prompt(`Type one more number`);
-   if (a===b){
-      
+   let max;
+   function maxNumber (a, b){
+      if (a > b) {
+         return a;
+      }
+      else if (b > a) {
+         return b;
+      }
+      else {
+         return `a=b`;
+      }
    }
-}
+   alert (maxNumber(a, b));
+
